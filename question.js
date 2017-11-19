00b93349d6f8139ec19a7af1fada7f691b4f722e
Anthony Catalfo <anthonycatalfo@gmail.com>
2017-11-19 07:59:16 +0000

    automatically backed up by learn

--- question.js
+++ question.js
@@ -1,1 +1,28 @@
 'use strict';
+class Question  {
+  constructor(name, email){
+    this.name = name
+    this.email = email
+    this.save()
+  }
+
+  save(){
+    this.constructor._All.push(this)
+  }
+
+  static All() {
+    return this._All;
+  }
+
+  static FindByEmail(email){
+    return this.All().filter(function(user){
+      return user.email === email;
+    })
+  }
+}
+User._All = []
+
+let sarah = new User("Sarah", "sarah@flatironschool.com")
+let bob = new User("Bob", "bob@flatironschool.com")
+
+let matchingUser = User.FindByEmail("sarah@flatironschool.com")
